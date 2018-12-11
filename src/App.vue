eb3ea1071a773b0489d3446cb3219c253515932b
watsonwu9 <wuyisince927@gmail.com>
2018-12-11 17:56:01 +0800

    add wx.request

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script>
+import {get} from './util.js'
 export default {
-  created () {
+  async created () {
     // 调用API从本地缓存中获取数据
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    console.log('app created and cache logs by setStorageSync')
+    const res = await get('/weapp/demo')
+    console.log(res)
+    console.log('小吴，小程序启动了')
   }
 }
 </script>
