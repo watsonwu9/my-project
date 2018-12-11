de80fc51d3c67504087dd186927b9ef19da21cf1
watsonwu9 <wuyisince927@gmail.com>
2018-12-11 14:26:22 +0800

    startover

---
+++ src/pages/books/Book.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    personal book  
+  </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ src/pages/me/Me.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    personal data  
+  </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style>
+
+</style>
